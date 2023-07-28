25567ad9eb1b9c7f31dd744cab6189f15836b8eb
H1Utku <139493659+H1Utku@users.noreply.github.com>
2023-07-28 20:04:51 +0300

    Word selector.
    extended word selector.

---
+++ Kelime.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL" : "Komik bir şeye verilen cevap",
+            "ROFL" : "bir şakaya karşılık cevap",
+            "SHEESH" : "onaylamamak"
+            "CREEPY"  "korkunç",
+            "AGGRO" : "agresifleşmek",
+            "WOW" : "hayranlık duymak",
+            }
+word = input("Anlamını bilmek istediğiniz bir kelimeyi büyük harflerle yazın ")
+if word in meme_dict.keys("LOL"):
+    print("Komik bir şeye verilen cevap")
+else :
+    print("bu kelime sözlükte yok")
---
+++ kelime2.py
@@ -0,0 +1,17 @@
+word = input("Anlamını bilmek istediğiniz bir kelimeyi büyük harflerle yazın ")
+if word in("LOL"):
+    print("Komik bir şeye verilen cevap")
+elif word in("CRINGE"):
+    print("Garip ya da utandırıcı bir şey")
+elif word in("ROFL"):
+    print("bir şakaya karşılık cevap")
+elif word in("SHEESH"):
+    print("onaylamamak")
+elif word in("CREEPY"):
+    print("korkunç")
+elif word in("AGGRO"):
+    print("agresifleşmek")
+elif word in("WOW"):
+    print("hayranlık duymak")
+else:
+    print("bu kelime sözlükte yok")
